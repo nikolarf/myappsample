8d8ed6f7b7831dc2e6003f00e0fb357597051427
Nikola Rafajlovic <nikolaraf@hotmail.com>
2019-04-01 16:12:00 +0200

    Welcome changed
    Welcome page title change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Blog
                 </div>
 
                 <div class="links">
